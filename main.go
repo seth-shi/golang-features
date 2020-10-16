3059784feaa067c908dbf8b78cb6339a15ebd6a9
seth-shi <shiguopeng@minyea.com>
2020-10-16 17:31:45 +0800

    增加服务端口开放

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "golang-functions/routes"
+import (
+	"github.com/joho/godotenv"
+	"golang-functions/routes"
+	"log"
+	"os"
+)
+
+func init()  {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 
 	router := routes.RegisterRoutes()
 
-	router.Run("0.0.0.0:8888")
+	router.Run("0.0.0.0:" + os.Getenv("SERVE_PORT"))
 }
--- routes/web.go
+++ routes/web.go
@@ -1,8 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	. "golang-functions/controllers"
+)
 
 func webRoutes(router *gin.Engine)  {
 
 
+	router.GET("ping", Ping)
 }
